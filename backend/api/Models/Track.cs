0cfad85140a2f5e972a9a30d90d880fb6be4d992
realpj10101 <parsajfr1385oo@gmail.com>
2025-06-14 13:11:03 +0330

    create track mode

--- backend/api/Models/Track.cs
+++ backend/api/Models/Track.cs
@@ -1,5 +1,13 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace api.Models;
 
-public record Track (
-    
+public record Track(
+    [property: BsonId, BsonRepresentation(BsonType.ObjectId)]
+    ObjectId Id,
+    string UploaderName,
+    string FileName,
+    byte[] FileData,
+    DateTime UploadedAt
 );
