48f8c1e2f462f1b6d20876ec895f7da013bac3a8
realpj10101 <parsajfr1385oo@gmail.com>
2025-06-14 12:25:13 +0330

    add api call for login

--- client/src/app/components/account/login/login.component.ts
+++ client/src/app/components/account/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AccountService } from '../../../services/account.service';
+import { LoggedInUser, Login } from '../../../models/account.model';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +12,46 @@ import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, 
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private _accountService = inject(AccountService);
+  private _fb = inject(FormBuilder);
+
   password: string = '';
   hide: boolean = true;
+  wrongUserNameOrPassword: string | undefined;
+
+  loginFg = this._fb.group({
+    emailCtrl: ['', [Validators.required, Validators.maxLength(50), Validators.pattern(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$/)]],
+    passwordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]],
+  })
 
   togglePassword(): void {
     this.hide = !this.hide;
+  }
+
+  get EmailCtrl(): FormControl {
+    return this.loginFg.get('emailCtrl') as FormControl;
+  }
+
+  get PasswordCtrl(): FormControl {
+    return this.loginFg.get('passwordCtrl') as FormControl;
+  }
+
+  login(): void {
+    console.log('ok');
+    
+    let loginPlayer: Login = {
+      email: this.EmailCtrl.value,
+      password: this.PasswordCtrl.value
+    }
+
+    this._accountService.loginPlayer(loginPlayer).subscribe({      
+      next: (loggedInPlayer: LoggedInUser | null) => {
+        console.log(loggedInPlayer);
+      },
+      // show wrong username or password error.
+      error: err => {
+        this.wrongUserNameOrPassword = err.error;
+      }
+    })
   }
 }
