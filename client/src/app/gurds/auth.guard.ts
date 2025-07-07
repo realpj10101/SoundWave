b3abfe1867017ca017881cfbd4a62d26d766cdb7
realpj10101 <parsajfr1385oo@gmail.com>
2025-07-07 11:09:15 +0330

    create auth gaurd

--- client/src/app/gurds/auth.guard.ts
+++ client/src/app/gurds/auth.guard.ts
@@ -1,5 +1,30 @@
-import { CanActivateFn } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const snackBar = inject(MatSnackBar);
+  const router = inject(Router);
+  const platfromId = inject(PLATFORM_ID);
+
+  if (isPlatformBrowser(platfromId)) {
+    const loggedInUserStr: string | null = localStorage.getItem('loggedInUser');
+
+    if (loggedInUserStr) {
+      return true;
+    }
+
+    snackBar.open('Please login first.', 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+      duration: 7000
+    });
+
+    localStorage.setItem('returnUrl', state.url);
+
+    router.navigate(['account/login'], { queryParams: { 'returnUrl': state.url } });
+  }
+
+  return false;
 };
