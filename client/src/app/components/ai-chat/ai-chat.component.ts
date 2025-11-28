d324c3ebb78fead7b695e5ed932b686009c19c0e
realpj10101 <parsajfr1385oo@gmail.com>
2025-11-28 14:51:20 +0330

    Created ai chat component

---
+++ client/src/app/components/ai-chat/ai-chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-chat',
+  imports: [],
+  templateUrl: './ai-chat.component.html',
+  styleUrl: './ai-chat.component.scss'
+})
+export class AiChatComponent {
+
+}
