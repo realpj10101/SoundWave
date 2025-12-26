cf895401820a3efcfb27a53964e450e2e6bba0b9
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-26 22:08:30 +0330

    add main photo model and cover path property to audio model

--- client/src/app/models/audio.model.ts
+++ client/src/app/models/audio.model.ts
@@ -1,10 +1,16 @@
+import { MainPhoto } from "./photo.model";
+
 export interface Audio {
     uploaderName: string;
     fileName: string;
     filePath: string;
+    coverPath: MainPhoto;
     uploadedAt: Date;
     isLiking: boolean;
     isAdding: boolean;
     likersCount: number;
     addersCount: number;
+    genres: [];
+    moods: [];
+    tags: [];
 }
--- client/src/app/models/photo.model.ts
+++ client/src/app/models/photo.model.ts
@@ -5,3 +5,9 @@ export interface Photo {
     url_enlarged: string;
     isMain: boolean;
 }
+
+export interface MainPhoto {
+    url_165: string;
+    url_256: string;
+    url_enlarged: string;
+}
