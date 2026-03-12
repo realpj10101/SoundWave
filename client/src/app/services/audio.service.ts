ea5c46c87ce1a9cc8b788c900a0a719e659d05a2
realpj10101 <parsajfr1385oo@gmail.com>
2026-03-13 03:23:21 +0330

    update audio service

--- client/src/app/services/audio.service.ts
+++ client/src/app/services/audio.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { PaginationHandler } from '../extensions/paginationHandler';
 import { AudioParams } from '../models/helpers/audio-params';
-import { Observable } from 'rxjs';
+import { Observable, retry } from 'rxjs';
 import { PaginatedResult } from '../models/helpers/paginatedResult';
 import { Audio } from '../models/audio.model';
 
@@ -13,7 +13,7 @@ import { Audio } from '../models/audio.model';
 export class AudioService {
   private _http = inject(HttpClient);
 
-  private readonly _apiUrl = environment.apiUrl + 'api/audiofile';
+  private readonly _apiUrl = environment.apiUrl + 'api/audiofile/';
   private paginationHendler = new PaginationHandler();
 
   getAll(audioParams: AudioParams): Observable<PaginatedResult<Audio[]>> {
@@ -25,7 +25,23 @@ export class AudioService {
   getUserAudios(audioParams: AudioParams): Observable<PaginatedResult<Audio[]>> {
     const params = this.getHttpParams(audioParams);
     
-    return this.paginationHendler.getPaginatedResult<Audio[]>(this._apiUrl + '/get-user-audios', params);
+    return this.paginationHendler.getPaginatedResult<Audio[]>(this._apiUrl + 'get-user-audios', params);
+  }
+
+  stream(id: string): Observable<HttpEvent<Blob>> {
+    return this._http.get<Blob>(this._apiUrl + 'stream/' + id, {
+      responseType: 'blob' as 'json',
+      observe: 'events',
+      reportProgress: true
+    })
+  }
+
+  goToNextAudio(id: string): Observable<Audio> {
+    return this._http.get<Audio>(this._apiUrl + 'next-audio/' + id);
+  }
+
+  goToPrevious(id: string): Observable<Audio> {
+    return this._http.get<Audio>(this._apiUrl + 'previous-audio/' + id);
   }
 
   private getHttpParams(audioParams: AudioParams): HttpParams {
